95944646d7915772025f53d7dffb1a8331cc58eb
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2020-06-14 17:54:27 -0400

    Rework configuration, rename 30-extra to 30-apps

--- 20-gui.sh
+++ 20-gui.sh
@@ -1,12 +1,14 @@
 AddPackage adobe-source-han-sans-jp-fonts # Adobe Source Han Sans Subset OTF - Japanese OpenType/CFF fonts
+AddPackage arc-gtk-theme # A flat theme with transparent elements for GTK 3, GTK 2 and Gnome-Shell
+AddPackage cups # The CUPS Printing System - daemon package
+AddPackage cups-pdf # PDF printer for cups
 AddPackage dmenu # Generic menu for X
 AddPackage feh # Fast and light imlib2-based image viewer
-AddPackage gparted # A Partition Magic clone, frontend to GNU Parted
 AddPackage i3-wm # An improved dynamic tiling window manager
+AddPackage kde-gtk-config # GTK2 and GTK3 Configurator for KDE
 AddPackage kitty # A modern, hackable, featureful, OpenGL-based terminal emulator
 AddPackage kscreen # KDE's screen management software
 AddPackage lxappearance # Feature-rich GTK+ theme switcher of the LXDE Desktop
-AddPackage nemo # Cinnamon file manager (Nautilus fork)
 AddPackage otf-ipafont # Japanese outline fonts by Information-technology Promotion Agency, Japan (IPA)
 AddPackage paprefs # Configuration dialog for PulseAudio
 AddPackage pavucontrol # PulseAudio Volume Control
@@ -18,7 +20,43 @@ AddPackage scrot # Simple command-line screenshot utility for X
 AddPackage sddm # QML based X11 and Wayland display manager
 AddPackage ttf-font-awesome # Iconic font designed for Bootstrap
 AddPackage ttf-joypixels # Emoji as a Service (formerly EmojiOne)
+AddPackage xorg-docs # X.org documentations
+AddPackage xorg-fonts-100dpi # X.org 100dpi fonts
+AddPackage xorg-fonts-75dpi # X.org 75dpi fonts
+AddPackage xorg-iceauth # ICE authority file utility
+AddPackage xorg-luit # Filter that can be run between an arbitrary application and a UTF-8 terminal emulator
 AddPackage xorg-server # Xorg X server
+AddPackage xorg-server-devel # Development files for the X.Org X server
+AddPackage xorg-server-xephyr # A nested X server that runs as an X application
+AddPackage xorg-server-xnest # A nested X server that runs as an X application
+AddPackage xorg-server-xvfb # Virtual framebuffer X server
+AddPackage xorg-server-xwayland # run X clients under wayland
+AddPackage xorg-sessreg # Register X sessions in system utmp/utmpx databases
+AddPackage xorg-smproxy # Allows X applications that do not support X11R6 session management to participate in an X11R6 session
+AddPackage xorg-x11perf # Simple X server performance benchmarker
+AddPackage xorg-xbacklight # RandR-based backlight control application
+AddPackage xorg-xcmsdb # Device Color Characterization utility for X Color Management System
+AddPackage xorg-xcursorgen # Create an X cursor file from PNG images
+AddPackage xorg-xdpyinfo # Display information utility for X
+AddPackage xorg-xdriinfo # Query configuration information of DRI drivers
+AddPackage xorg-xev # Print contents of X events
+AddPackage xorg-xgamma # Alter a monitor's gamma correction
+AddPackage xorg-xhost # Server access control program for X
+AddPackage xorg-xinit # X.Org initialisation program
+AddPackage xorg-xinput # Small commandline tool to configure devices
+AddPackage xorg-xkbevd # XKB event daemon
+AddPackage xorg-xkbutils # XKB utility demos
+AddPackage xorg-xkill # Kill a client by its X resource
+AddPackage xorg-xlsatoms # List interned atoms defined on server
+AddPackage xorg-xlsclients # List client applications running on a display
+AddPackage xorg-xmodmap # Utility for modifying keymaps and button mappings
+AddPackage xorg-xpr # Print an X window dump from xwd
+AddPackage xorg-xrandr # Primitive command line interface to RandR extension
+AddPackage xorg-xrefresh # Refresh all or part of an X screen
+AddPackage xorg-xvinfo # Prints out the capabilities of any video adaptors associated with the display that are accessible through the X-Video extension
+AddPackage xorg-xwd # X Window System image dumping utility
+AddPackage xorg-xwininfo # Command-line utility to print information about windows on an X server
+AddPackage xorg-xwud # X Window System image undumping utility
 
 AddPackage --foreign equilux-theme # A couple of full-dark, neutral-gray themes (for GTK2, GTK3, Shell, and GDM) using the Equilux palette.
 AddPackage --foreign opensiddur-hebrew-fonts # The Open Siddur Project's Unicode Hebrew font pack. A large collection of open source Hebrew fonts, as well as a few for Latin, Greek, Cyrillic, Arabic, and Amharic
---
+++ 30-apps.sh
@@ -0,0 +1,8 @@
+AddPackage firefox # Standalone web browser from mozilla.org
+AddPackage gparted # A Partition Magic clone, frontend to GNU Parted
+AddPackage nemo # Cinnamon file manager (Nautilus fork)
+AddPackage seahorse # GNOME application for managing PGP keys.
+AddPackage vlc # Multi-platform MPEG, VCD/DVD, and DivX player
+
+AddPackage --foreign google-chrome # The popular and trusted web browser by Google (Stable Channel)
+AddPackage --foreign spotify # A proprietary music streaming service
