cea309d66e5fc517f431128ded55aca612bcfa24
Aaron Ky-Riesenbach <aaronkyriesenbach@pobox.com>
2020-07-12 17:57:14 -0400

    Add 00-ignored.sh

---
+++ 00-ignored.sh
@@ -0,0 +1,73 @@
+IgnorePackage dpkg # The Debian Package Manager tools
+IgnorePackage grub # GNU GRand Unified Bootloader
+IgnorePackage handbrake-cli # Multithreaded video transcoder (CLI)
+IgnorePackage intel-ucode # Microcode update files for Intel CPUs
+IgnorePackage iwd # Internet Wireless Daemon
+IgnorePackage libdvdcss # Portable abstraction library for DVD decryption
+IgnorePackage mariadb # Fast SQL database server, derived from MySQL
+IgnorePackage mysql-workbench # A cross-platform, visual database design tool developed by MySQL
+IgnorePackage networkmanager # Network connection manager and user applications
+IgnorePackage nodejs # Evented I/O for V8 javascript
+IgnorePackage npm # A package manager for javascript
+IgnorePackage nvidia # NVIDIA drivers for linux
+IgnorePackage openvpn # An easy-to-use, robust and highly configurable VPN (Virtual Private Network)
+IgnorePackage opera # A fast and secure web browser
+IgnorePackage sublime-text # Sublime Text is a sophisticated text editor for code, markup and prose
+IgnorePackage telegram-desktop # Official Telegram Desktop client
+IgnorePackage xf86-video-vesa # X.org vesa video driver
+
+IgnorePackage --foreign anydesk-bin # The Fast Remote Desktop Application (Generic based package)
+IgnorePackage --foreign brother-hl2270dw # Brother HL-2270DW CUPS Driver
+IgnorePackage --foreign checkra1n # checkra1n is a semi-tethered jailbreak, developed primarily by Luca Todesco (qwertyoruiop). It's based on the checkm8 bootrom exploit released by axi0mx. checkra1n supports iOS 12.3 and newer.
+IgnorePackage --foreign epson-inkjet-printer-201212w # Epson printer driver (WF-3010, WF-3520, WF-3530, WF-3540)
+IgnorePackage --foreign filebot # The ultimate TV and Movie Renamer
+IgnorePackage --foreign jetbrains-toolbox # Manage all your JetBrains Projects and Tools
+IgnorePackage --foreign ncurses5-compat-libs # System V Release 4.0 curses emulation library, ABI 5
+IgnorePackage --foreign nvm # Node Version Manager - Simple bash script to manage multiple active node.js versions
+IgnorePackage --foreign postman-bin # Build, test, and document your APIs faster
+IgnorePackage --foreign slack-desktop # Slack Desktop (Beta) for Linux
+IgnorePackage --foreign spicetify-cli # Command-line tool to customize Spotify client
+IgnorePackage --foreign webcamoid # Webcamoid is a full featured webcam capture application.
+IgnorePackage --foreign zoom # Video Conferencing and Web Conferencing Service
+
+IgnorePath '/boot/*'
+IgnorePath '/etc/.pwd.lock'
+IgnorePath '/etc/.updated'
+IgnorePath '/etc/adjtime'
+IgnorePath '/etc/arch-release'
+IgnorePath '/etc/ca-certificates/*'
+IgnorePath '/etc/cups/*'
+IgnorePath '/etc/environment'
+IgnorePath '/etc/fstab'
+IgnorePath '/etc/group'
+IgnorePath '/etc/group-'
+IgnorePath '/etc/gshadow'
+IgnorePath '/etc/gshadow-'
+IgnorePath '/etc/hostname'
+IgnorePath '/etc/hosts'
+IgnorePath '/etc/iwd'
+IgnorePath '/etc/ld.so.cache'
+IgnorePath '/etc/locale.conf'
+IgnorePath '/etc/locale.gen'
+IgnorePath '/etc/localtime'
+IgnorePath '/etc/machine-id'
+IgnorePath '/etc/mkinitcpio.d/linux.preset'
+IgnorePath '/etc/modprobe.d/*'
+IgnorePath '/etc/openvpn/*'
+IgnorePath '/etc/os-release'
+IgnorePath '/etc/pacman.d/gnupg/*'
+IgnorePath '/etc/pacman.d/mirrorlist'
+IgnorePath '/etc/pam.d/*'
+IgnorePath '/etc/passwd'
+IgnorePath '/etc/passwd-'
+IgnorePath '/etc/printcap'
+IgnorePath '/etc/shadow'
+IgnorePath '/etc/shadow-'
+IgnorePath '/etc/shells'
+IgnorePath '/etc/ssl/*'
+IgnorePath '/etc/sudoers'
+IgnorePath '/etc/systemd/*'
+IgnorePath '/lost+found'
+IgnorePath '/opt/spotify/*'
+IgnorePath '/usr/*'
+IgnorePath '/var/*'
